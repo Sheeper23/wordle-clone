c4f5a1ff70f9bc6576e52799759447733edb7a7c
Jai Misquith <jai.misquith@gmail.com>
2023-09-06 17:33:14 -0500

    error fix

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,7 +2,7 @@ export default function TopBar() {
     return (
         <header className="flex mb-[2%] items-center justify-center border-b border-white">
             <div className="flex w-full max-w-5xl justify-center mx-2 my-3">
-                <p className="font-bold text-xl text-white">Reactle</p>
+                <p className="font-bold text-xl text-white">Wordle</p>
             </div>
         </header>
     )
