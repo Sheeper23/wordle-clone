b706c18bbd200c8fdd8cb5d961b9009c2036aa98
Jai Misquith <jai.misquith@gmail.com>
2023-09-11 21:33:57 -0500

    github link

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+import { FaGithub } from 'react-icons/fa'
+
 export default function TopBar() {
     return (
         <header className="flex mb-[2%] items-center justify-center border-b border-borders">
-            <div className="flex w-full max-w-5xl justify-center mx-2 my-3">
+            <div className="flex w-full max-w-5xl justify-between mx-2 my-3 items-center">
+                <div>{/* placeholder */}</div>
                 <p className="font-bold text-5xl font-title text-white cursor-default">Wordle</p>
+                <Link className="hover:bg-cyan-400 rounded-full select-none" target="_blank" href={"https://github.com/Sheeper23"}>
+                    <FaGithub color="white" size="30" />
+                </Link>
             </div>
         </header>
     )
