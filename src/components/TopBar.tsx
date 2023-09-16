41ae1bfa81441cc82fd7798074066bf8ab3dd91b
Jai Misquith <jai.misquith@gmail.com>
2023-09-15 22:09:12 -0500

    github spin

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,7 +7,7 @@ export default function TopBar() {
             <div className="flex w-full max-w-5xl justify-between mx-2 my-3 items-center">
                 <div>{/* placeholder */}</div>
                 <p className="font-bold text-5xl font-title text-white cursor-default">Wordle</p>
-                <Link className="hover:bg-cyan-400 rounded-full select-none" target="_blank" href={"https://github.com/Sheeper23"}>
+                <Link className="hover:scale-110 hover:rotate-[360deg] rounded-full select-none transition-transform duration-300 ease-in-out" target="_blank" href={"https://github.com/Sheeper23"}>
                     <FaGithub color="white" size="30" />
                 </Link>
             </div>
